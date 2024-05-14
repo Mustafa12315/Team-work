fd9b3156b595ef2302a3efd5921a646807494313
Thomas Fischer (1000117937) <FISCTJ1@student.op.ac.nz>
2024-05-14 16:08:12 +1200

    this time

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Another_git_repository
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, again");
+        }
+    }
+}
